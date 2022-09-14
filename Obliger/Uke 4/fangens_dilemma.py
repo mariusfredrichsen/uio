b9c0b4ae91fd366a0a8a9ce58da58fad033d93b9
Bsian02 <mariusfredrichsen@gmail.com>
2022-09-14 10:12:16 +0200

    Master

--- Obliger/Uke 4/fangens_dilemma.py
+++ Obliger/Uke 4/fangens_dilemma.py
@@ -49,7 +49,7 @@ def utfor_spill():
     print(f"spiller1 sin score: {score1}\nspiller2 sin score: {score2}")
 
 def utfor_spill_uendelig():
-    runder = 10
+    runder = 20
     score1 = 0
     score2 = 0
     trekk1 = []
@@ -59,7 +59,7 @@ def utfor_spill_uendelig():
         for i in range(runder):
             trekk1.append(spill_slemt(trekk2))
             trekk2.append(spill_snilt(trekk1))
-            score = beregn_score(spill_slemt(trekk2), spill_snilt(trekk1))
+            score = beregn_score(spill_slemt(trekk2), min_strategi_ASDASD(trekk1,trekk2))
             score1 += score[0]
             score2 += score[1]
         print(f"spiller1 sin score: {score1}\nspiller2 sin score: {score2}")
@@ -69,11 +69,15 @@ def utfor_spill_uendelig():
 def min_strategi_ASDASD(dinevalg,minevalg):
     if dinevalg == [] or minevalg == []:
         return "samarbeid"
+    temp = []
+    if len(dinevalg) >= 1:
+        for i in range(len(dinevalg)-2,len(dinevalg)):
+            temp.append(dinevalg[i])
+        if temp.count("samarbeid") >= 1:
+            return "svik"
     if dinevalg[len(dinevalg)-2] == "svik":
         return "svik"
-    temp = []
-    if len(dinevalg) == 6:
-        for i in range(len(dinevalg)-6,len(dinevalg)):
-            temp.append(dinevalg[i])
-        if temp.count("samarbeid") >= 6:
-            return "svik"
+    else:
+        return "samarbeid"
+
+utfor_spill_uendelig()
