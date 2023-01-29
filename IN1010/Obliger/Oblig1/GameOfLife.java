f0c3db86e69983b3217eab8ad70c1625375eb7b6
Bsian02 <mariusfredrichsen@gmail.com>
2023-01-29 21:18:31 +0100

    Master

--- IN1010/Obliger/Oblig1/GameOfLife.java
+++ IN1010/Obliger/Oblig1/GameOfLife.java
@@ -1,9 +1,12 @@
+import java.util.Date;
+
 public class GameOfLife {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Verden verden = new Verden(8, 12);
         verden.tegn();
 
         for (int i = 0; i < 20; i++) {
+            Thread.sleep(500);
             verden.oppdatering();
             verden.tegn();
         }
