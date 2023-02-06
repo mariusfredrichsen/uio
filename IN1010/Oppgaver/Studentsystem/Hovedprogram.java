c671572cd0cec49c76faa6d3515c02c3b12732b3
Bsian02 <mariusfredrichsen@gmail.com>
2023-02-06 20:56:47 +0100

    modified:   IN1010/Oppgaver/Studentsystem/Hovedprogram.class 	modified:   IN1010/Oppgaver/Studentsystem/Hovedprogram.java 	modified:   IN1010/Oppgaver/Studentsystem/Studentsystem.class 	modified:   IN1010/Oppgaver/Studentsystem/Studentsystem.java 	deleted:    fil.txt

--- IN1010/Oppgaver/Studentsystem/Hovedprogram.java
+++ IN1010/Oppgaver/Studentsystem/Hovedprogram.java
@@ -1,10 +1,14 @@
-package IN1010.Oppgaver.Studentsystem;
-
 import java.io.FileNotFoundException;
 
 class Hovedprogram {
     public static void main(String[] args) throws FileNotFoundException {
         Studentsystem system = new Studentsystem();
         system.lesFil("emnestudenter.txt");
+        system.mestStudenter();
+        system.mestFag();
+        system.alleSomTarEtFag();
+        system.leggTilStudent("Ida","INF1000");
+        system.fjernStudent("Ida", "INF1000");
+        system.leggTilFag("IN1030");
     }
 }
