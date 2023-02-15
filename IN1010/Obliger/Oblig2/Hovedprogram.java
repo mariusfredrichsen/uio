d8a781d845b24542c85505a316a2af1c2f34897e
Bsian02 <mariusfredrichsen@gmail.com>
2023-02-15 23:31:39 +0100

    modified:   IN1010/Obliger/Oblig2/Hovedprogram.java 	modified:   IN1010/Obliger/Oblig2/Vanlig.java 	new file:   IN1010/Obliger/Oblig2/datastruktur.png

--- IN1010/Obliger/Oblig2/Hovedprogram.java
+++ IN1010/Obliger/Oblig2/Hovedprogram.java
@@ -3,20 +3,20 @@ package IN1010.Obliger.Oblig2;
 public class Hovedprogram {
     public static void main(String[] args) {
         //Legemidler her:
-        Narkotisk narkotisk1 = new Narkotisk("nark1", 100, 2.5, 1, 7);
-        Vanedannende vanedannende = new Vanedannende("vane", 200, 1, 2, 3);
-        Vanlig vanlig = new Vanlig("vanl", 50, 20, 3);
-        Narkotisk narkotisk2 = new Narkotisk("nark2", 100, 2.5, 4, 7);
+        Narkotisk narkotisk1 = new Narkotisk("nark1", 100, 2.5, 7);
+        Vanedannende vanedannende = new Vanedannende("vane", 200, 1, 3);
+        Vanlig vanlig = new Vanlig("vanl", 50, 20);
+        Narkotisk narkotisk2 = new Narkotisk("nark2", 100, 2.5, 7);
 
         //Leger her:
         Lege lege = new Lege("En lege");
         Spesialist spesialist = new Spesialist("Enda en lege", "mammaLikerBamserMedLitenM");
 
         //Resepter her:
-        BlaaResepter blaaResept = new BlaaResepter(1, narkotisk1, lege, 1, 30);
-        HviteResepter hvitResept = new HviteResepter(2, vanedannende, lege, 2, 4);
-        MilResept milResept = new MilResept(3, vanlig, spesialist, 3);
-        PResept pResept = new PResept(4, narkotisk2, spesialist, 4, 9);
+        BlaaResepter blaaResept = new BlaaResepter(narkotisk1, lege, 1, 30);
+        HviteResepter hvitResept = new HviteResepter(vanedannende, lege, 2, 4);
+        MilResept milResept = new MilResept(vanlig, spesialist, 3);
+        PResept pResept = new PResept(narkotisk2, spesialist, 4, 9);
 
         System.out.println(narkotisk1);
         System.out.println(vanedannende);
--- IN1010/Obliger/Oblig2/Vanlig.java
+++ IN1010/Obliger/Oblig2/Vanlig.java
@@ -1,7 +1,7 @@
 package IN1010.Obliger.Oblig2;
 
 public class Vanlig extends Legemiddel {
-    public Vanlig(String navn, int pris, double virkestoff, int id){
+    public Vanlig(String navn, int pris, double virkestoff){
         super(navn,pris,virkestoff);
     }
 }
