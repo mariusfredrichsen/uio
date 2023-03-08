539830dbd5fe503610fa4169cedcf5831e07b412
Bsian02 <mariusfredrichsen@gmail.com>
2023-03-08 15:47:48 +0100

    modified:   IN1010/Obliger/Oblig2/Spesialist.java

--- IN1010/Obliger/Oblig2/Spesialist.java
+++ IN1010/Obliger/Oblig2/Spesialist.java
@@ -1,10 +1,8 @@
 public class Spesialist extends Lege implements Godkjenningsfritak{
     String kontrollKode;
-    String navn;
 
     public Spesialist(String navn, String kontrollKode) {
         super(navn);
-        this.navn = navn;
         this.kontrollKode = kontrollKode;
     }
 
