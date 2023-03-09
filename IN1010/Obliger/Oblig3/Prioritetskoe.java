8724fac86a13baa24096c3434a43c2ccc6098d33
Bsian02 <mariusfredrichsen@gmail.com>
2023-03-09 20:13:15 +0100

    modified:   IN1010/Obliger/Oblig3/Prioritetskoe.java 	modified:   IN1010/Obliger/Oblig3/TestKoe.java 	new file:   IN1010/Oppgaver/EnkelArrayListe.java 	new file:   IN1010/Oppgaver/Katt/Katt.java new file:   IN1010/Oppgaver/Katt/Main.java 	new file:   IN1010/Oppgaver/Katt/SortertArrayList.java

--- IN1010/Obliger/Oblig3/Prioritetskoe.java
+++ IN1010/Obliger/Oblig3/Prioritetskoe.java
@@ -1,25 +1,19 @@
 public class Prioritetskoe <E extends Comparable <E>> extends Lenkeliste <E>{
-    /*@Override
+    /* @Override
     public void leggTil(E x) {
         super.leggTil(x);
-        
         for (int i = 0; i < super.stoerrelse(); i++) {
             Node nesteLenke = hode;
             for (int j = 0; j < super.stoerrelse(); j++) {
-                try {
-                    Node tempNesteLenke = nesteLenke.neste; 
-    
-                    E data1 = nesteLenke.data;
-                    E data2 = tempNesteLenke.data;
-                    if (data1.compareTo(data2) > 0) {
-                        nesteLenke.data = data2;
-                        nesteLenke = nesteLenke.neste;
-                        nesteLenke.data = data1;
-                    } 
+                Node tempNesteLenke = nesteLenke.neste; 
+                E data1 = nesteLenke.data;
+                E data2 = tempNesteLenke.data;
+                if (data1.compareTo(data2) > 0) {
+                    nesteLenke.data = data2;
                     nesteLenke = nesteLenke.neste;
-                } catch (NullPointerException e) {
-                    ;
+                    nesteLenke.data = data1;
                 }
+                nesteLenke = nesteLenke.neste;
             }
         }
     } */
@@ -35,22 +29,18 @@ public class Prioritetskoe <E extends Comparable <E>> extends Lenkeliste <E>{
             hode.neste = tempHolder;
         } else {
             Node nesteLenke = hode;
-            
             while (nesteLenke != null) {
                 if (nesteLenke.neste == null) {
                     super.leggTil(x);
                     break;
-                } else {
-                    if (nesteLenke.neste.data.compareTo(x) >= 0) {
-                        Node nesteLenkeHolder = nesteLenke.neste; 
-                        Node nyNode = new Node(x);
-                        nyNode.neste = nesteLenkeHolder;
-                        nesteLenke.neste = nyNode;
-                        break;
-                    } else {
-                        nesteLenke = nesteLenke.neste;
-                    }
                 }
+                if (nesteLenke.neste.data.compareTo(x) > 0) {
+                    Node nyNode = new Node(x);
+                    nyNode.neste = nesteLenke.neste;
+                    nesteLenke.neste = nyNode;
+                    break;
+                }
+                nesteLenke = nesteLenke.neste;
             }
         }
     }
---
+++ IN1010/Oppgaver/Katt/SortertArrayList.java
@@ -0,0 +1,13 @@
+package IN1010.Oppgaver.Katt;
+import java.util.ArrayList;
+
+public class SortertArrayList <E> {
+    
+    ArrayList<E> list = new ArrayList<>();
+
+    public void settInn(E data) {
+        list.add(data);
+    }
+
+    public void hentUt()
+}
