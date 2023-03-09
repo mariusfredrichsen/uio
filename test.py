3c2e3564830ac8314ca5c813d0d9cae8e2efe2d2
Bsian02 <mariusfredrichsen@gmail.com>
2023-03-09 20:14:49 +0100

    modified:   IN1010/Obliger/Oblig3/datastruktur.png 	modified:   IN1150/Obliger/Innlevering0.fdb_latexmk 	modified:   IN1150/Obliger/Innlevering0.fls 	modified:   IN1150/Obliger/Innlevering0.log 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-a = 4
-b = 5
+input_string = input().strip().split()
 
-print("asdasd", a+b)
+for i in range(len(input_string)):
+    input_string[i] = int(input_string[i])
+
+print(abs(input_string[0]-input_string[1]))
