73d4a80311a2f3fe0417f16f5e88fb74ce116da9
Bsian02 <mariusfredrichsen@gmail.com>
2023-03-22 13:35:25 +0100

    modified:   IN1010/Oppgaver/Uke9/NteTall.java 	modified:   IN1010/Obliger/Oblig4 (modified content)

--- IN1010/Oppgaver/Uke9/NteTall.java
+++ IN1010/Oppgaver/Uke9/NteTall.java
@@ -2,7 +2,9 @@ package IN1010.Oppgaver.Uke9;
 
 public class NteTall {
     public static void main(String[] args) {
-        Teller etEllerAnnet = new Teller(5, 1000);
-        new Thread(etEllerAnnet).start();
+        for (int i = 0; i < 10; i++) {
+            Teller etEllerAnnet = new Teller(i, 1000);
+            new Thread(etEllerAnnet).start();
+        }
     }
 }
