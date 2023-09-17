b0d0bcaeba153c52e397e56a27ed8359999f1176
Bsian02 <mariusfredrichsen@gmail.com>
2023-09-17 17:17:00 +0200

    modified:   IN2010/Oblig1/countswaps.py

--- IN2010/Oblig1/countswaps.py
+++ IN2010/Oblig1/countswaps.py
@@ -3,10 +3,7 @@ class CountSwaps(list):
 
     def swap(self, i, j):
         self.swaps += 1
-        self[i], self[j] = self[j], self[i]        
-
-    def swap_merge(self):
-        self.swaps += 1
+        self[i], self[j] = self[j], self[i]
 
     def swap_merge(self):
         self.swaps += 1
