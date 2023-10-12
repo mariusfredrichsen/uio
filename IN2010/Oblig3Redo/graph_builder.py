a4442040d3f36f5387aaba5dbb01df241c2a9737
Bsian02 <mariusfredrichsen@gmail.com>
2023-10-12 23:03:00 +0200

    new file:   IN2010/Oblig3Redo/actors.tsv 	modified:   IN2010/Oblig3Redo/graph_builder.py 	modified:   IN2010/Oblig3Redo/marvel_graph 	modified:   IN2010/Oblig3Redo/marvel_graph.svg 	new file:   IN2010/Oblig3Redo/movies.tsv

--- IN2010/Oblig3Redo/graph_builder.py
+++ IN2010/Oblig3Redo/graph_builder.py
@@ -4,16 +4,20 @@ from collections import defaultdict
 
 
 def build_graph(lines_actors, lines_movies):
-    V, E, w = set(), defaultdict(set), dict() # w tar formen (v, u, f) = r der hvor v og u er skuespillere og f er en filmid og r er vurderingen
+    V, E, w = set(), defaultdict(set), defaultdict(set) # w tar formen (v, u, f) = r der hvor v og u er skuespillere og f er en filmid og r er vurderingen
     for v in lines_actors:
         V.add(v)
+        print(lines_actors[v])
         for u in lines_actors:
             if v != u:
                 for m in lines_actors[v][1]:
                     if m in lines_actors[u][1]:
                         E[v].add(u)
-                        w[(v, u)] = (m, lines_movies[m][1])
-                        w[(u, v)] = (m, lines_movies[m][1])
+                        if m not in lines_movies:
+                            continue
+                        w[(v, u)].add((m, float(lines_movies[m][1])))
+                        w[(u, v)].add((m, float(lines_movies[m][1])))
+    
     return V, E, w
 
 def draw_graph(G):
@@ -25,7 +29,7 @@ def draw_graph(G):
         for u in E[v]:
             if (v, u) not in visited_edges:
                 visited_edges.add((u, v))
-                dot.edge(v, u, label=w[(u, v)][0] + " " + w[(u, v)][1])
+                dot.edge(v, u, label=str(len(w[(u, v)])))
             
     dot.render('marvel_graph', format='svg')
         
@@ -43,7 +47,7 @@ def shortest_paths_from(G, v):
     
     return parents
 
-def shortest_paths_from_to(G, v, u):
+def shortest_path_from_to(G, v, u):
     parents = shortest_paths_from(G, v)
     path = [u]
     
@@ -57,34 +61,85 @@ def print_path(G, actors, movies, path):
     _, _, w = G
     print("\n\n")
     for i in range(len(path)-1):
-        m, r = w[(path[i],path[i+1])]
-        print(f"{actors[path[i]][0]} ===[ {movies[m][0]} ({r}) ]===>")
+        for m in w[(path[i],path[i+1])]:
+            break
+
+        print(f"{actors[path[i]][0]} ===[ {movies[m[0]][0]} ({m[1]}) ]===>")
     print(actors[path[-1]][0] + "\n\n")
+    
+def least_effort_paths_from(G, v):
+    V, E, w = G
+    queue = [(0, v)]
+    dist = defaultdict(lambda: float('inf'))
+    dist[v] = 0
+    parents = {v: None}
+    
+    while queue:
+        r, v = queue.pop(0)
+        if r != dist[v]:
+            continue
+        for u in E[v]:
+            m = find_best_film(w[(v, u)])
+            tmp_r = r + 10 - m[1]
+            if tmp_r < dist[u]:
+                dist[u] = tmp_r
+                queue.append((tmp_r, u))
+                parents[u] = (v, m)
+            
+    return parents
 
+def least_effort_path_from_to(G, v, u):
+    parents = least_effort_paths_from(G, v)
+    path = [u]
 
+    while parents[u]:
+        path.insert(0,parents[u])
+        u = parents[u][0]
+    
+    return path
 
+def find_best_film(movies):
+    best_rated = 0
+    best_movie = ()
+    for m in movies:
+        if m[1] > best_rated:
+            best_rated = m[1]
+            best_movie = m
+    return best_movie
 
+def print_least_weighted_path(G, actors, movies, path):
+    V, E, w = G
+    for i in range(len(path)-1):
+        print(f"{actors[path[i][0]][0]} ===[ {movies[path[i][1][0]][0]} ({path[i][1][1]}) ]===>")
+    print(f"{actors[path[-1]][0]}")
+    
 def main():
     lines_actors = dict()
     lines_movies = dict()
-    with open('marvel_actors.tsv', encoding='utf8') as f:
+    with open('actors.tsv', encoding='utf8') as f:
         for line in f:
             line = line.strip().split("\t")
             line = [line[0], line[1], [e for e in line[2:]]]
             lines_actors[line[0]] = (line[1], line[2])
-    with open('marvel_movies.tsv', encoding='utf8') as f:
+    with open('movies.tsv', encoding='utf8') as f:
         for line in f:
             line = line.strip().split("\t")
             lines_movies[line[0]] = (line[1], line[2])
 
     G = build_graph(lines_actors, lines_movies)
-    draw_graph(G)
+    # draw_graph(G)
     
+    print_path(G, lines_actors, lines_movies, shortest_path_from_to(G, 'nm2255973', 'nm0000460'))
+    print_path(G, lines_actors, lines_movies, shortest_path_from_to(G, 'nm0424060', 'nm8076281'))
+    print_path(G, lines_actors, lines_movies, shortest_path_from_to(G, 'nm4689420', 'nm0000365'))
+    print_path(G, lines_actors, lines_movies, shortest_path_from_to(G, 'nm0000288', 'nm2143282'))
+    print_path(G, lines_actors, lines_movies, shortest_path_from_to(G, 'nm0637259', 'nm0931324'))
     
-    
-    
-    path = shortest_paths_from_to(G, 'nm0000949', 'nm0000313')
-    print_path(G, lines_actors, lines_movies, path)
+    print_least_weighted_path(G, lines_actors, lines_movies, least_effort_path_from_to(G, 'nm2255973', 'nm0000460'))
+    print_least_weighted_path(G, lines_actors, lines_movies, least_effort_path_from_to(G, 'nm0424060', 'nm8076281'))
+    print_least_weighted_path(G, lines_actors, lines_movies, least_effort_path_from_to(G, 'nm4689420', 'nm0000365'))
+    print_least_weighted_path(G, lines_actors, lines_movies, least_effort_path_from_to(G, 'nm0000288', 'nm2143282'))
+    print_least_weighted_path(G, lines_actors, lines_movies, least_effort_path_from_to(G, 'nm0637259', 'nm0931324'))
     
         
 main()
