3f50abcd3ed8d8024ce13c1739058cef5698f94f
Bsian02 <mariusfredrichsen@gmail.com>
2024-09-05 19:42:25 +0200

    asd

--- IN5320/Assigment1/assignment1b.js
+++ IN5320/Assigment1/assignment1b.js
@@ -1,36 +1,53 @@
+var listElements = []
+const uListElement = document.getElementById("cl")
 
-function add_country(country) {
-    const countryList = document.getElementById("countries");
-    const newListItem = document.createElement("li");
-    const countryNode = document.createTextNode(country);
-
-    newListItem.appendChild(countryNode)
-
-    const countryButton = document.createElement("button");
-    const countryButtonText = document.createTextNode("X");
-
-    countryButton.appendChild(countryButtonText)
-    countryButton.onclick = function () {
-        countryList.removeChild(newListItem);
-        countryButton.remove();
-    }
-
-    countryButton.onmouseenter = function () {
-        countryButton.style.background = "red";
-    }
-    countryButton.onmouseleave = function () {
-        countryButton.style.background = "lightgray";
-    }
-
-    newListItem.appendChild(countryButton)
-
-    countryList.appendChild(newListItem)
+function addCountryButton() {
+    const inputElement = document.getElementById('ci')
+    listElements.push(inputElement.value)
+    inputElement.value = ""
+    updateList()
 }
 
-function get_input() {
-    const inputElement = document.getElementById('inputCountry')
-    const inputValue = inputElement.value
-    inputElement.value = ""
+function addCountry(country) {
+    // create list node
+    const listNode = document.createElement("li")
+    const textNode = document.createTextNode(country)
 
-    return inputValue
+    listNode.appendChild(textNode)
+
+    // create button that deletes corresponding list element
+    const button = document.createElement("button")
+    button.textContent = "X"
+    button.onclick = () => {
+        textNode.remove()
+        button.remove()
+        listNode.remove()
+        listElements.map((listElement) => listElement === listNode)
+    }
+    button.onmouseenter = () => {
+        button.style = "background: red;"
+    }
+    button.onmouseleave = () => {
+        button.style = "background: lightgray;"
+    }
+
+    listNode.appendChild(button)
+
+    uListElement.appendChild(listNode)
+}
+
+function checkStartOfCountry(country, searchWord) {
+    return country.startsWith(searchWord)
+}
+
+function filterList(list, searchWord) {
+    return list.filter((x) => checkStartOfCountry(x, searchWord))
+}
+
+function updateList() {
+    uListElement.innerHTML = ""
+
+    for (listElement of listElements) {
+        addCountry(listElement)
+    }
 }
